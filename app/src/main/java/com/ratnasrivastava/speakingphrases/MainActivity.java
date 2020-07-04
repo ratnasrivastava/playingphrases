f65440af9888d0bd0558653a6e9863b8dd922313
Ratna Srivastava <er.ratnasrivastava@gmail.com>
2020-07-04 17:46:10 +0530

    initial commit

---
+++ app/src/main/java/com/ratnasrivastava/speakingphrases/MainActivity.java
@@ -0,0 +1,26 @@
+package com.ratnasrivastava.speakingphrases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void playPhrase(View view) {
+        Button button = (Button) view;
+        Log.d("unique7", ""+button.getTag().toString());
+        //play phrase by getTag
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(button.getTag().toString(), "raw", getPackageName()));
+        mediaPlayer.start();
+    }
+}
